0e49160dc3f7622bb489d3fbb5581fae99e9f6ab
SivaGoud.T <tirukala.goud@zee.esselgroup.com>
2019-09-04 11:31:14 +0530

    finished frame work

---
+++ MyFirstPod/MyFirstPod/MyFirstPod.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstPod.h
+//  MyFirstPod
+//
+//  Created by ThandavaSiva on 04/09/19.
+//  Copyright © 2019 ThandavaSiva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFirstPod.
+FOUNDATION_EXPORT double MyFirstPodVersionNumber;
+
+//! Project version string for MyFirstPod.
+FOUNDATION_EXPORT const unsigned char MyFirstPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstPod/PublicHeader.h>
+
+
---
+++ MyFirstPod/MyFirstPod/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  MyFirstPod
+//
+//  Created by ThandavaSiva on 04/09/19.
+//  Copyright © 2019 ThandavaSiva. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    private init(){}
+    public static func doSomthing() -> String {
+        return "Did some stuff, here you"
+    }
+}
